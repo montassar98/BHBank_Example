7f016a3ffbeae9af0718bea7d9f0c507b5d8c5df
montassar98 <montassarselmi372@gmail.com>
2020-10-12 16:01:23 +0100

    working on StatisticsFragment.kt implementing charts

--- app/src/main/java/com/vneuron/bhbankexample/main/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/vneuron/bhbankexample/main/fragments/StatisticsFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.vneuron.bhbankexample.R
+import com.yabu.livechart.model.DataPoint
+import com.yabu.livechart.model.Dataset
+import com.yabu.livechart.view.LiveChart
 
 class StatisticsFragment(): Fragment() {
     override fun onCreateView(
@@ -15,7 +18,13 @@ class StatisticsFragment(): Fragment() {
     ): View? {
         val view: View? = inflater.inflate(R.layout.fragment_statistics, container, false)
 
+        val liveChart = view?.findViewById<LiveChart>(R.id.live_chart)
+        val dataSet = Dataset(mutableListOf(DataPoint(0f, 1f),
+            DataPoint(1f, 3f),
+            DataPoint(2f, 6f)
+        ))
 
+        liveChart?.setDataset(dataSet)?.drawYBounds()?.drawBaseline()?.drawFill()?.drawDataset()
 
         return view
     }
